eec087cfa1cf63238da690506b0db008eec23f79
Tommy Nguyen <tuannvm@hotmail.com>
2026-01-19 13:06:55 -0800

    fix(constants): change default pdfFolder from PDFs to pdfs
    Signed-off-by: Tommy Nguyen <tuannvm@hotmail.com>

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -29,7 +29,7 @@ export const DEFAULT_SETTINGS: ViwoodsSettings = {
     imagesFolder: 'Attachments',
     audioFolder: 'Attachments',
     strokesFolder: 'Attachments',
-    pdfFolder: 'PDFs',
+    pdfFolder: 'pdfs',
 
     // Import options
     dateFormat: 'YYYY-MM-DD HH:mm:ss',
